5a884ca880d0942682954545f1fafed1afe07b27
ip48 <innapolak@gmail.com>
2025-01-31 04:32:55 -0500

    change logo, background, About me etc

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -5,25 +5,22 @@ function AboutUs() {
   return (
     <div className="about-us-container">
       {/* <h1 className="about-us-heading">About Us</h1> */}
-      <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity!</p>
+      <p className="about-us-description">Welcome to Green Nature, and see the beauty of Nature!</p>
       <p className="about-us-content">
-        At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of 
-        high-quality plants that not only enhance the beauty of your surroundings but also contribute to a healthier and 
-        more sustainable lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every 
-        plant enthusiast.
+        At Green Nature, we are passionate about bringing the beauty of nature closer to you. 
+        Our mission is to offer a diverse selection of high-quality plants that not only enhance your surroundings but also promote a healthier,more sustainable lifestyle. 
+        Whether you're looking for air-purifying greenery or beautifully fragrant blooms, we have something for every plant enthusiast.
       </p>
-      {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
+      {/* <p className="plant_logo_left"><img src="https://cdn.pixabay.com/photo/2017/06/27/18/22/sunflowers-2448351_960_720.png" height='50px' width='50px' alt="" /></p> */}
       <p className="about-us-content">
-        Our team of experts is dedicated to ensuring that each plant meets our strict standards of quality and care. 
-        Whether you're a seasoned gardener or just starting your green journey, we're here to support you every step of 
-        the way. Feel free to explore our collection, ask questions, and let us help you find the perfect plant for your 
-        home or office.
+        We are committed to providing the highest quality plants, adhering to the strictest standards. 
+        Whether you're a seasoned gardener or just beginning your green journey, we're here to support you every step of the way. 
+        Explore our collection, ask questions, and let us help you find the perfect plant for your home or office.
       </p>
-      {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
+      {/* <p className="plant_logo_right"><img src="https://cdn.pixabay.com/photo/2017/06/27/18/22/sunflowers-2448351_960_720.png" height='50px' width='50px' alt="" /></p> */}
 
       <p className="about-us-content">
-        Join us in our mission to create a greener, healthier world. Visit Paradise Nursery today and experience the 
-        beauty of nature right at your doorstep.
+        Enjoy our heaven!
       </p>
     </div>
   );
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -274,11 +274,11 @@ const handlePlantsClick = (e) => {
              <div className="navbar" style={styleObj}>
             <div className="tag">
                <div className="luxury">
-               <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
+               <img src="https://cdn.pixabay.com/photo/2017/06/27/18/22/sunflowers-2448351_960_720.png" alt="" />
                <a href="/" style={{textDecoration:'none'}}>
                         <div>
-                    <h3 style={{color:'white'}}>Paradise Nursery</h3>
-                    <i style={{color:'white'}}>Where Green Meets Serenity</i>
+                    <h3 style={{color:'white'}}>Green Nature</h3>
+                    <i style={{color:'white'}}>See the beauty of Nature</i>
                     </div>
                     </a>
                 </div>
